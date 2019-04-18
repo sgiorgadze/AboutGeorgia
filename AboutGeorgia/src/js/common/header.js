ed806871bbe19b1b2d39e7a7995d55d4ca547840
sgiorgadze <salome.giorgadze86@gmail.com>
2019-04-18 23:53:13 +0400

    dato

--- AboutGeorgia/src/js/common/header.js
+++ AboutGeorgia/src/js/common/header.js
@@ -9,11 +9,11 @@ var AboutGergia = (function()
         
         function showHideTopHeader() {
             if (document.body.scrollTop > 38 || document.documentElement.scrollTop > 38) {
-                document.getElementById("header-top").classList.remove("show");
+                document.getElementById("header-top").classList.remove("show1");
                 document.getElementById("header-bottom").classList.add("darken");
                 //document.getElementById("logo").style.fontSize = "25px";
             } else {
-                document.getElementById("header-top").classList.add("show");
+                document.getElementById("header-top").classList.add("show1");
                 document.getElementById("header-bottom").classList.remove("darken");
             }
         }
